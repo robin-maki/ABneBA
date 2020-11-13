b97743527e1dc0c6d624b819b3fec3114a0f0f05
robin* <robin_maki@planet.moe>
2020-11-13 22:42:31 +0900

    매직넘버 상수화

--- unit.js
+++ unit.js
@@ -2,26 +2,29 @@
 맵크기: 
 */
 // 상위: 시야 제공
+const SPEED = 2;
+const TURN_SPEED = 0.1;
+
 class Unit {
     constructor(position) {
         this.hp = 100;
         this.position = position; // vector {x,y}
         this.direction = 0; // angle(radian), -PI < direction <= PI
-        this.size = 50; // radius
+        this.size = 10; // radius
     }
-    goStraight(speed=10) { // execute once per frame
-        if (speed > 10) {
-            speed = 10;
+    goStraight(speed=SPEED) { // execute once per frame
+        if (speed > SPEED) {
+            speed = SPEED;
         }
         this.position.x += Math.cos(this.direction)*speed;
         this.position.y += Math.sin(this.direction)*speed;
     }
     turn(speed) { // execute once per frame
-        if(speed < -0.1) {
-            speed = -0.1;
+        if(speed < -TURN_SPEED) {
+            speed = -TURN_SPEED;
         }
-        else if(speed > 0.1) {
-            speed = 0.1;
+        else if(speed > TURN_SPEED) {
+            speed = TURN_SPEED;
         }
         this.direction += speed;
         if(this.direction <= -Math.PI) {
