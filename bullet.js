44f65bcadf750671cbf88e258d134717a5d38128
ivoire5204 <49222346+ivoire5204@users.noreply.github.com>
2020-11-14 04:00:46 +0900

    kill unit

--- bullet.js
+++ bullet.js
@@ -1,10 +1,15 @@
 const gameManager = require('./gameManager');
 
+const SPEED = 100;
+const SIZE = 1;
+
 class Bullet {
     constructor(position, angle, damage = 20) {
         this.position = position;
         this.angle = angle;
         this.damage = damage;
+        this.size = SIZE;
+        this.speed = SPEED;
         gameManager.addBullet(this);
     }
 }
--- gameManager.js
+++ gameManager.js
@@ -7,16 +7,28 @@ const MAP_SIZE = 5000;
 let frame;
 let units = [];
 let bullets = [];
-let count = [0, 0];
+let count = [0, 0]; /// left: red / right: blue
 let mapSize = 5000; // radius
 
 setInterval(() => {
     frame++;
     mapSize--;
-    // scan units out of map
-    units.forEach((unit) => {
+    units.forEach((unit, index) => {
+        // scan units out of map
         if( Math.sqrt(Math.pow(unit.position.x,2) + Math.pow(unit.position.y,2)) - unit.SIZE >= mapsize ) {
             unit.hp -= (Math.floor(frame/900) + 1) * 1;
+        }
+        // kill units
+        if (unit.hp <= 0) {
+            switch (unit.team){
+                case "RED":
+                    count[0]--;
+                    break;
+                case "BLUE":
+                    count[1]--;
+                    break;
+            }
+            units.splice(index, 1);
         }
     });
 }, 1000 / FPS);
@@ -33,7 +45,15 @@ module.exports = {
             let u = new Unit({
                 x: Math.cos(rad) * Math.random() * mapSize,
                 y: Math.sin(rad) * Math.random() * mapSize
-            });
+            }, "RED");
+            units.push(u);
+        }
+        for(let i = 0; i < TEAM_POP; i++) {
+            const rad = Math.random() * Math.PI * 2;
+            let u = new Unit({
+                x: Math.cos(rad) * Math.random() * mapSize,
+                y: Math.sin(rad) * Math.random() * mapSize
+            }, "BLUE");
             units.push(u);
         }
     },
