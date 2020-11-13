2b1292002d83c9cc13cfe75a3a01c2701af59557
robin* <robin_maki@planet.moe>
2020-11-13 22:33:44 +0900

    Unit.turn 메소드 수정

--- unit.js
+++ unit.js
@@ -2,11 +2,11 @@
 맵크기: 
 */
 // 상위: 시야 제공
-class unit {
+class Unit {
     constructor(position) {
         this.hp = 100;
         this.position = position; // vector {x,y}
-        this.direction = 0; // angle(radian)
+        this.direction = 0; // angle(radian), -PI < direction <= PI
         this.size = 50; // radius
     }
     goStraight(speed=10) { // execute once per frame
@@ -16,11 +16,20 @@ class unit {
         this.position.x += Math.cos(this.direction)*speed;
         this.position.y += Math.sin(this.direction)*speed;
     }
-    turn(speed=0.1) { // execute once per frame
-        if (speed > 0.1) {
+    turn(speed) { // execute once per frame
+        if(speed < -0.1) {
+            speed = -0.1;
+        }
+        else if(speed > 0.1) {
             speed = 0.1;
         }
-        this.direction += angle;
+        this.direction += speed;
+        if(this.direction <= -Math.PI) {
+            this.direction += 2 * Math.PI;
+        }
+        else if(this.direction > Math.PI) {
+            this.direction -= 2 * Math.PI;
+        }
     }
     shoot() {
 
