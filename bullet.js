e677e2b01da11febee6e69a1fe9a171c552e2acc
ivoire5204 <49222346+ivoire5204@users.noreply.github.com>
2020-11-14 04:23:24 +0900

    bullet 프레임당 판정 추가

--- bullet.js
+++ bullet.js
@@ -1,15 +1,19 @@
 const gameManager = require('./gameManager');
 
-const SPEED = 100;
-const SIZE = 1;
+const SPEED = 10;
+const LIFE = 10;
 
 class Bullet {
     constructor(position, angle, damage = 20) {
         this.position = position;
         this.angle = angle;
         this.damage = damage;
-        this.size = SIZE;
-        this.speed = SPEED;
-        gameManager.addBullet(this);
+        this.life = LIFE;
+        gameManager.addBullet(this);    //todo
     }
 }
+
+Bullet.LIFE = LIFE;
+Bullet.SPEED = SPEED;
+
+module.exports = Bullet;
